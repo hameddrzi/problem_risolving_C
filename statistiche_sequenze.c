e3834d098a0499759109358f797fbfced85d4447
Hamed <hamed.2012.darzi@gmail.com>
2024-08-27 15:53:12 +0200

    add function

--- statistiche_sequenze.c
+++ statistiche_sequenze.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+
+bool leg_sequenze(int *somma ,int *somma_sq, int *max ){
+    int count = 0, n;
+    *somma = *somma_sq = *max = 0;
+
+
+
+}
+
 int main(){
-    int n;
-    printf("insert numer: ");
-    scanf("%d", &n);
-    if(n>0){
-        printf("%d\n", n);
+    int somma, somma_sq, max;
+
+    while(leg_sequenze(&somma, &somma_sq, &max)){
+        printf("%d %d %d", somma, somma_sq, max);
     }
 
 
