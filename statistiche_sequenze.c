7c0e3a33395c87c93b78302ad1b929a810832645
Hamed <hamed.2012.darzi@gmail.com>
2024-08-27 15:59:23 +0200

    complet code

--- statistiche_sequenze.c
+++ statistiche_sequenze.c
@@ -6,9 +6,16 @@ bool leg_sequenze(int *somma ,int *somma_sq, int *max ){
     int count = 0, n;
     *somma = *somma_sq = *max = 0;
 
-
-
+    while (scanf("%d", &n)&& n!=0){
+        ++count;
+        *somma += n;
+        *somma_sq += n * n;
+        if(n > *max)
+            *max = n;
+    }
+    return (count > 0);
 }
+
 
 int main(){
     int somma, somma_sq, max;
@@ -16,8 +23,4 @@ int main(){
     while(leg_sequenze(&somma, &somma_sq, &max)){
         printf("%d %d %d", somma, somma_sq, max);
     }
-
-
-
-
 }
