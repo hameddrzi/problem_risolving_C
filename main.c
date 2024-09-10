b3d6b35a822f08a280227f1921ef71fbb1779a3c
Hamed <hamed.2012.darzi@gmail.com>
2024-09-10 16:08:30 +0200

    writed main()

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
-#include <stdio.h>
+//https://leetcode.com/problems/palindrome-number/
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPalindrome(int x) {
+    
+}
+
+#define MAX_NUM 50
+
+int main(){
+    int N[MAX_NUM +1];
+    scanf("%50s", &N );
+    if(isPalindrome(N)){
+        printf("palindrome");
+    }
 }
