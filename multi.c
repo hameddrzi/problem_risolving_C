c4bbc9ae5bd87454684831eb786490d5d9948b49
Hamed <hamed.2012.darzi@gmail.com>
2024-10-18 17:43:22 +0200

    completed

--- multi.c
+++ multi.c
@@ -1,48 +1,63 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     int row, cols, row2, cols2 ;
-    printf("insert Rows of Matrice1: \n");
+    printf("insert Rows of Matrice1: ");
     scanf("%d", &row);
-    printf("insert Cols of Matrice1: \n");
+    printf("insert Cols of Matrice1: ");
     scanf("%d", &cols);
-    printf("now insert row of matrice2: \n");
+
+
+    printf("now insert row of matrice2: ");
     scanf("%d", &row2);
-    printf("insert Cols of Matrice2: \n");
+    printf("insert Cols of Matrice2: ");
     scanf("%d", &cols2);
 
 
-    int matrice1[row][cols];
-    int matrice2[row2][cols2];
-    int i,j;
-        if(cols == row2) {
-                for (i = 0; i < row; ++i) {
-                    for (j = 0; j < cols; ++j) {
-                        printf("insert 1 Matrice[%d][%d]: \n", i + 1, j + 1);
-                        scanf("%d", &matrice1[i][j]);
-                    }
-                }
-                for (i = 0; i < row2; ++i) {
-                    for (int j = 0; j < cols2; ++j) {
-                        printf("insert 2 Matrice[%d][%d]: \n", i + 1, j + 1);
-                        scanf("%d", &matrice2[i][j]);
-                     }
-                }
-                int multi[row][cols2];
-                for (i = 0; i < row; ++i) {
-                    for (int j = 0; j < cols2; ++j) {
-                        for (int k = 0; k <cols; ++k) {
-                            multi[i][j] += matrice1[i][k] * matrice2[k][j];
-                        }
 
-                    }
-                }
-            for (int i = 0; i < row; ++i) {
-                for (int j = 0; j < cols2; ++j) {
-                    printf("%d ", multi[i][j]);
-                }
-                printf("\n");
+    if(cols == row2) {
+        int matrice1[row][cols];
+        int matrice2[row2][cols2];
+        int multi[row][cols2];
+
+
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < cols2; ++j) {
+                multi[i][j] = 0;
             }
-
         }
+
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                printf("insert 1 Matrice[%d][%d]: ", i + 1, j + 1);
+                scanf("%d", &matrice1[i][j]);
+            }
+        }
+        for (int i = 0; i < row2; ++i) {
+            for (int j = 0; j < cols2; ++j) {
+                printf("insert 2 Matrice[%d][%d]: ", i + 1, j + 1);
+                scanf("%d", &matrice2[i][j]);
+            }
+        }
+
+
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < cols2; ++j) {
+                for (int k = 0; k < cols; ++k) {
+                    multi[i][j] += matrice1[i][k] * matrice2[k][j];
+                }
+            }
+        }
+        printf("risult of moltiplicazione\n");
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < cols2; ++j) {
+                printf("%d ", multi[i][j]);
+            }
+            printf("\n");
+        }
+
+    }else{
+        printf("Not multiplicable ");
+    }
 }
